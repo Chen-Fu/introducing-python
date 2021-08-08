1f4b2e909dcb649c33e51cd777a2278c32b27da3
Chen-Fu <chenfu.liao@gmail.com>
2021-08-09 03:37:11 +0800

    Create things to do Ch5.py

---
+++ things to do Ch5.py
@@ -0,0 +1,24 @@
+#5.1 Capitalize the word starting with m:
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
+print(song.upper())
+
+#5.2 Print each list question with its correctly matching answer, in the form:
+questions = [
+"We don't serve strings around here. Are you a string?",
+"What is said on Father's Day in the forest?",
+"What makes the sound 'Sis! Boom! Bah!'?"
+]
+answers = [
+"An exploding sheep.",
+"No, I'm a frayed knot.",
+"'Pop!' goes the weasel."
+]
+
+print(questions[0])
+print(answers[1])
+print(questions[1])
+print(answers[2])
+print(questions[2])
+print(answers[1])
